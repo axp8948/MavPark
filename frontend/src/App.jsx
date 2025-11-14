14acf097e9b266045b8f59c7096160885fd38810
Anmol Pandey <axp8948@mavs.uta.edu>
2025-11-14 13:36:40 -0600

    Frontend Chnages

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">MavPark 🅿️</h1>
-      <p className="text-gray-600 mt-2">Tailwind setup successful!</p>
-    </div>
-  );
-}
-export default App;
